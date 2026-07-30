c418260fac4f7c8cc4f029c1ac6cf614b189278e
nhathuych <nhathuych@gmail.com>
2026-07-30 18:40:00 +0700

    Gracefull shutdown

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -22,7 +27,32 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server error: %v", err)
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("Server listening on %s", server.Addr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Printf("Error starting the server: %v", err)
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to %v signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Could not stop the server gracefully: %v", err)
+
+			if closeErr := server.Close(); closeErr != nil {
+				log.Printf("Could not force close the server: %v", closeErr)
+			}
+		}
+		log.Println("Server stopped gracefully")
 	}
 }
